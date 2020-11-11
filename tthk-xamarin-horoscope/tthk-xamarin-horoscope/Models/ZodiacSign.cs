f37cf498f6198ac0dff55bc98ba23993cf60f315
Nikolas Laus <49353043+blinchk@users.noreply.github.com>
2020-11-11 22:43:47 +0200

    Добавьте файлы проекта.

---
+++ tthk-xamarin-horoscope/tthk-xamarin-horoscope/Models/ZodiacSign.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace tthk_xamarin_horoscope.Models
+{
+    class ZodiacSign
+    {
+        string title;
+        string description;
+        TimeSpan startDate;
+        TimeSpan endDate;
+        ImageSource picture;
+
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+
+        public string Description
+        {
+            get => description;
+            set => description = value;
+        }
+
+        public TimeSpan StartDate
+        {
+            get => startDate;
+            set => startDate = value;
+        }
+
+        public TimeSpan EndDate
+        {
+            get => endDate;
+            set => endDate = value;
+        }
+
+        public ImageSource Picture
+        {
+            get => picture;
+            set => picture = value;
+        }
+    }
+}
