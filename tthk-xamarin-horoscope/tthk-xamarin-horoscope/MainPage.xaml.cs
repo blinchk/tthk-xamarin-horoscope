af130645ee92a930df76e9faafba39e44d0b2050
Nikolas Laus <49353043+blinchk@users.noreply.github.com>
2020-11-17 20:31:59 +0200

    Change aquarius.txt coding.

--- tthk-xamarin-horoscope/tthk-xamarin-horoscope/MainPage.xaml.cs
+++ tthk-xamarin-horoscope/tthk-xamarin-horoscope/MainPage.xaml.cs
@@ -23,23 +23,17 @@ namespace tthk_xamarin_horoscope
             zodiacSignPicker.ItemsSource = zodiacSigns.Select(zs => zs.Title).ToArray(); // Get zodiac signs' names using Linq
             zodiacSignPicker.SelectedIndexChanged += ZodiacSignPicker_SelectedIndexChanged;
             zodiacSignDatePicker.DateSelected += ZodiacSignDatePicker_DateSelected;
+            int todayZodiacSignIndex = GetZodiacSignIndexByDate(DateTime.Today);
+            DisplayZodiacSignInfo(todayZodiacSignIndex);
+            zodiacSignDatePicker.Date = DateTime.Today;
+            zodiacSignPicker.SelectedIndex = todayZodiacSignIndex;
         }
 
         private void ZodiacSignDatePicker_DateSelected(object sender, DateChangedEventArgs e)
         {
-            int selectedZodiacSignIndex;
-            if (e.NewDate.Month == 1 && e.NewDate.Day < 21 || e.NewDate.Month == 12 && e.NewDate.Day > 21)
-            {
-                selectedZodiacSignIndex = 0;
-                
-            }
-            else
-            {
-                var selectedZodiacSign = zodiacSigns.First(zs => e.NewDate.DayOfYear <= zs.EndDate.DayOfYear && e.NewDate.DayOfYear >= zs.StartDate.DayOfYear);
-                selectedZodiacSignIndex = zodiacSigns.IndexOf(selectedZodiacSign);
-            }
-            zodiacSignPicker.SelectedIndex = selectedZodiacSignIndex;
-            DisplayZodiacSignInfo(selectedZodiacSignIndex);
+            int index = GetZodiacSignIndexByDate(e.NewDate);
+            zodiacSignPicker.SelectedIndex = index;
+            DisplayZodiacSignInfo(index);
         }
 
         private void ZodiacSignPicker_SelectedIndexChanged(object sender, EventArgs e)
@@ -47,6 +41,20 @@ namespace tthk_xamarin_horoscope
             Picker picker = sender as Picker;
             zodiacSignDatePicker.Date = zodiacSigns[picker.SelectedIndex].StartDate + TimeSpan.FromDays(15);
             DisplayZodiacSignInfo(picker.SelectedIndex);
+        }
+
+        private int GetZodiacSignIndexByDate(DateTime date)
+        {
+            if (date.Month == 1 && date.Day < 21 || date.Month == 12 && date.Day > 21)
+            {
+                return 0;
+                
+            }
+            else
+            {
+                var selectedZodiacSign = zodiacSigns.First(zs => date.DayOfYear <= zs.EndDate.DayOfYear && date.DayOfYear >= zs.StartDate.DayOfYear);
+                return zodiacSigns.IndexOf(selectedZodiacSign);
+            }
         }
 
         private void DisplayZodiacSignInfo(int index)
